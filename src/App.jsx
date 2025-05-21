a338d71d4318bae83cad7b7fb6172c2ee5cdf5a6
santino <ing.roquezegarra@gmail.com>
2025-05-21 08:25:25 -0500

    fix22

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Login } from './components/Login';
 import { HomePage } from './pages/HomePage';
-import { CartPage } from './pages/CartPage';
+import { CartPage } from './pages/CardPage';
 import { PrivateRoute } from './components/PrivateRoute';
 import { Routes, Route } from "react-router-dom"; // 👈 NO uses BrowserRouter aquí
 import './App.css';
