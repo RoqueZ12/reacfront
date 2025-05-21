d711c9a9459e1357d0e476e6ab94597d6e532477
santino <ing.roquezegarra@gmail.com>
2025-05-21 00:24:15 -0500

    fix componente

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { NavBar } from "../components/NavBar";
+import { Cards} from "../components/Card";
 const HomePage = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
