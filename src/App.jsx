aabc7b96ac97ed68287643704428350c4588c5f7
santino <ing.roquezegarra@gmail.com>
2025-05-21 08:17:46 -0500

    add privateroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Login } from './components/Login';
 import { HomePage } from './pages/HomePage';
 import { CartPage } from './pages/CartPage';
+import { PrivateRoute } from './components/PrivateRoute';
 import { Routes, Route } from "react-router-dom"; // 👈 NO uses BrowserRouter aquí
 import './App.css';
 
