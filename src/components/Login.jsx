3152cd3d9db10c154aa1e577a5d101e6d0d234f4
santino <ing.roquezegarra@gmail.com>
2025-05-21 07:07:15 -0500

    add css login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,45 @@
 // src/components/Login.jsx
 import { GoogleButton } from "./GoogleBtn";
-
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "../styles/login.css"
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-2xl mb-4 font-bold">Login</h1>
-      <GoogleButton />
+    <div className="login-wrapper">
+      <div className="login-card">
+        <h2>Mini E-Commerce</h2>
+      
+
+        <form >
+          <label>Correo electrónico:</label>
+          <input
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="correo@ejemplo.com"
+          />
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="********"
+          />
+          <button type="submit" className="login-btn">Iniciar sesión</button>
+          
+        </form>
+        <button id="register" className="login-btn" onClick={() => navigate("/register")} style={{ marginTop: '10px' }}>Registrate</button>
+        <div className="divider">o</div>
+
+       
+        <GoogleButton />
+      </div>
     </div>
   );
 };
